4f8aa0be67373eda87c6d0ee5d35339ff6e93b96
Robert Douglas <138529257+robertdouglasaimon@users.noreply.github.com>
2024-03-26 00:21:42 -0300

    UPDATE

---
+++ exercicios externos/ex02/index.js
@@ -0,0 +1,22 @@
+let a = document.querySelector('img');
+
+function myfunon() {
+    a.style.animationDuration = "1s"; 
+}
+
+function myfunoff() {
+    a.style.animationDuration = "0s"; 
+}
+
+function myfun1() {
+    a.style.animationDuration = "1s";
+}
+
+function myfun2() {
+    a.style.animationDuration = "0.5s";
+}
+
+function myfun3() {
+    a.style.animationDuration = "0.1s";
+}
+
